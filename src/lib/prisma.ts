0cd019064a2d13988b97c62350997ebe74082eeb
Benito Pedro <benitopedro21@outlook.com>
2025-04-11 14:55:52 -0300

    feat: add Prisma migration commands and update database schema for user authentication support

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,3 @@
-// import { PrismaClient } from '@prisma/client'
 import {PrismaClient} from '~/prisma/app/generated/prisma/client';
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
