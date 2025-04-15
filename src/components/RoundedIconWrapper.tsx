31c70a023561d47fd487f3d53ebd566e819a50f7
Benito Pedro <benitopedro21@outlook.com>
2025-04-15 09:16:54 -0300

    📦 FIX: Update border class in RoundedIconWrapper and clean up buttonVariants

--- src/components/RoundedIconWrapper.tsx
+++ src/components/RoundedIconWrapper.tsx
@@ -7,7 +7,7 @@ const RoundedIconWrapper = ({children}: PropsWithChildren) => {
   // TODO: Background gradient effect
   return (
     <div className="rounded-icon-wrapper w-[96px] h-[96px] flex justify-center items-center">
-      <div className="w-[64px] h-[64px] flex justify-center items-center rounded-full border border-text-soft-200">
+      <div className="w-[64px] h-[64px] flex justify-center items-center rounded-full border border-bg-soft-200">
         {children}
       </div>
     </div>
