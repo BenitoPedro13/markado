d61963b66bd1daf3da59c342a6830312df571d7f
marcaumdesign <navalservice1@gmail.com>
2025-04-15 20:25:21 -0300

    📦 UPDATE: Refatorar o componente Service para melhorar a estrutura do código, incluindo a adição de um link para navegação e a reorganização de imports. Além disso, a lógica de renderização foi simplificada, mantendo a funcionalidade de compartilhamento e edição de serviços.

--- src/components/services/Service.tsx
+++ src/components/services/Service.tsx
@@ -1,6 +1,6 @@
-"use client"
+'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import * as Badge from '@/components/align-ui/ui/badge';
 import {
   RiCodeLine,
@@ -17,8 +17,9 @@ import * as ButtonGroup from '@/components/align-ui/ui/button-group';
 import * as Tooltip from '@/components/align-ui/ui/tooltip';
 import * as Switch from '@/components/align-ui/ui/switch';
 import * as Dropdown from '@/components/align-ui/ui/dropdown';
-import { useServices } from '@/contexts/ServicesContext';
-import { useRouter } from 'next/navigation';
+import {useServices} from '@/contexts/ServicesContext';
+import {useRouter} from 'next/navigation';
+import Link from 'next/link';
 
 type ServiceProps = {
   title: string;
@@ -29,7 +30,7 @@ type ServiceProps = {
 };
 
 function Service({title, slug, duration, price, status}: ServiceProps) {
-  const { updateServiceStatus } = useServices();
+  const {updateServiceStatus} = useServices();
   const [isEnabled, setIsEnabled] = useState(status === 'active');
   const router = useRouter();
   useEffect(() => {
@@ -41,72 +42,80 @@ function Service({title, slug, duration, price, status}: ServiceProps) {
     setIsEnabled(!isEnabled);
     updateServiceStatus(slug, newStatus);
   };
-
   return (
-    <div className="p-4 flex" onClick={() => {
-      router.push(`/services/${slug}`);
-    }}>
-      {/* Leading */}
-      <div className="w-full flex flex-col gap-2">
-        <div className="flex gap-2  items-center">
-          <span className="text-paragraph-lg text-text-strong-950">
-            {title}
-          </span>
-          <span className="text-paragraph-sm text-text-sub-600">{slug}</span>
+    <Link
+      href={`services/${slug}`}
+      className="w-full "
+    >
+      <div className="p-4 flex hover:bg-bg-weak-50 transition-colors duration-200">
+        {/* Leading */}
+        <div className="w-full flex flex-col gap-2">
+          <div className="flex gap-2  items-center">
+            <span className="text-paragraph-lg text-text-strong-950">
+              {title}
+            </span>
+            <span className="text-paragraph-sm text-text-sub-600">{slug}</span>
+          </div>
+          <div className="flex gap-2 items-center">
+            <Badge.Root variant="light" color="blue" size="medium">
+              <Badge.Icon as={RiTimeLine} />
+              {duration > 60
+                ? `${Math.floor(duration / 60)}h ${duration % 60}m`
+                : `${duration}m`}
+            </Badge.Root>
+            <span className="text-paragraph-md text-text-strong-950">
+              R$ {price}
+            </span>
+          </div>
         </div>
+        {/* Trailing */}
         <div className="flex gap-2 items-center">
-          <Badge.Root variant="light" color="blue" size="medium">
-            <Badge.Icon as={RiTimeLine} />
-            {duration > 60
-              ? `${Math.floor(duration / 60)}h ${duration % 60}m`
-              : `${duration}m`}
-          </Badge.Root>
-          <span className="text-paragraph-md text-text-strong-950">
-            R$ {price}
-          </span>
+          <Switch.Root
+            checked={isEnabled}
+            onCheckedChange={handleSwitchChange}
+          />
+          <ButtonGroup.Root>
+            <Tooltip.Root>
+              <Tooltip.Trigger asChild>
+                <ButtonGroup.Item>
+                  <ButtonGroup.Icon as={RiShare2Line} />
+                </ButtonGroup.Item>
+              </Tooltip.Trigger>
+              <Tooltip.Content size="small">
+                Compartilhar serviço
+              </Tooltip.Content>
+            </Tooltip.Root>
+            <Tooltip.Root>
+              <Tooltip.Trigger asChild>
+                <ButtonGroup.Item>
+                  <ButtonGroup.Icon as={RiFileCopyFill} />
+                </ButtonGroup.Item>
+              </Tooltip.Trigger>
+              <Tooltip.Content size="small">
+                Copiar link do serviço
+              </Tooltip.Content>
+            </Tooltip.Root>
+            <Dropdown.Root>
+              <Dropdown.Trigger asChild>
+                <ButtonGroup.Item>
+                  <ButtonGroup.Icon as={RiMore2Fill} />
+                </ButtonGroup.Item>
+              </Dropdown.Trigger>
+              <Dropdown.Content align="end" className="w-fit">
+                <Dropdown.Item>
+                  <Dropdown.ItemIcon as={RiDeleteBinLine} />
+                  Excluir serviço
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <Dropdown.ItemIcon as={RiPencilLine} />
+                  Editar serviço
+                </Dropdown.Item>
+              </Dropdown.Content>
+            </Dropdown.Root>
+          </ButtonGroup.Root>
         </div>
       </div>
-      {/* Trailing */}
-      <div className="flex gap-2 items-center">
-        <Switch.Root checked={isEnabled} onCheckedChange={handleSwitchChange} />
-        <ButtonGroup.Root>
-          <Tooltip.Root>
-          <Tooltip.Trigger asChild>
-            <ButtonGroup.Item>
-              <ButtonGroup.Icon as={RiShare2Line} />
-            </ButtonGroup.Item>
-          </Tooltip.Trigger>
-          <Tooltip.Content size="small">Compartilhar serviço</Tooltip.Content>
-        </Tooltip.Root>
-        <Tooltip.Root>
-          <Tooltip.Trigger asChild>
-            <ButtonGroup.Item>
-              <ButtonGroup.Icon as={RiFileCopyFill} />
-            </ButtonGroup.Item>
-          </Tooltip.Trigger>
-          <Tooltip.Content size="small">Copiar link do serviço</Tooltip.Content>
-        </Tooltip.Root>
-        <Dropdown.Root>
-        <Dropdown.Trigger asChild>
-          <ButtonGroup.Item>
-            <ButtonGroup.Icon as={RiMore2Fill} />
-          </ButtonGroup.Item>
-        </Dropdown.Trigger>
-        <Dropdown.Content align='end' className='w-fit'>
-          <Dropdown.Item>
-            <Dropdown.ItemIcon as={RiDeleteBinLine} />
-            Excluir serviço
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <Dropdown.ItemIcon as={RiPencilLine} />
-            Editar serviço
-          </Dropdown.Item>
-          
-        </Dropdown.Content>
-        </Dropdown.Root>
-        </ButtonGroup.Root>
-      </div>
-    </div>
+    </Link>
   );
 }
 
