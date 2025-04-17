b5e58d00b1a8a69bff47818a77420483b307d4ed
Benito Pedro <benitopedro21@outlook.com>
2025-04-17 00:15:50 -0300

    📦 ADD: Implement responsive Logo component and integrate it into Sidebar. Update Sidebar and SidebarFooter for improved layout and user experience with collapsible functionality using Zustand for state management. #95

--- src/components/navigation/SidebarFooter.tsx
+++ src/components/navigation/SidebarFooter.tsx
@@ -3,10 +3,12 @@
 import Image from 'next/image';
 import SidebarFooterSkeleton from './SidebarFooterSkeleton';
 import { useSessionStore } from '@/providers/session-store-provider';
+import { useSidebarStore } from '@/store/useSidebarStore';
 
 export default function SidebarFooter() {
   const user = useSessionStore((state) => state.user);
   const isLoading = useSessionStore((state) => state.isLoading);
+  const {isCollapsed, toggleCollapse} = useSidebarStore();
 
   if (isLoading) {
     return <SidebarFooterSkeleton />;
@@ -17,19 +19,19 @@ export default function SidebarFooter() {
   }
 
   return (
-    <div className="p-4 w-full">
-      <div className="flex items-center gap-3">
+    <div className="p-4 w-full overflow-hidden">
+      <div className={`flex items-center gap-3 ${isCollapsed ? 'justify-center' : ''}`}>
         {user.image && (
           <Image
             src={user.image}
             alt={user.name || 'User'}
-            width={40}
-            height={40}
-            className="rounded-full"
+            width={isCollapsed ? 48 : 40}
+            height={isCollapsed ? 48 : 40}
+            className="rounded-full transition-all duration-300"
             priority
           />
         )}
-        <div className='flex flex-col items-start'>
+        <div className={`flex flex-col items-start ${isCollapsed ? 'hidden' : ''}`}>
           <p className="text-text-strong-950 text-label-sm">{user.name}</p>
           <p className="text-text-sub-600 text-paragraph-xs">{user.email}</p>
         </div>
