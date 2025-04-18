77d2c96e12c219f4a8b992dcddb0f3c3db8a68f8
marcaumdesign <navalservice1@gmail.com>
2025-04-18 09:49:48 -0300

    ✨ ADICIONAR: Atualizar a página de disponibilidade para incluir a lista de disponibilidade e ajustar o layout, melhorando a apresentação das informações.

--- src/app/availability/page.tsx
+++ src/app/availability/page.tsx
@@ -1,18 +1,22 @@
+'use client';
+
 import PageLayout from '@/components/PageLayout';
-import {useTranslations} from 'next-intl';
 import Header from '@/components/navigation/Header';
 import * as Divider from '@/components/align-ui/ui/divider';
+import AvailabilityList from '@/components/availability/AvailabilityList';
 
 /** Availability page of the website. */
 export default function AvailabilityPage() {
-  const t = useTranslations('IndexPage');
-
   return (
-    <PageLayout title="Home">
-      {/* <Header variant="scheduling"/> */}
-      <Header variant="availability" mode="inside" />
-      <Divider.Root />
-      {/* <Home /> */}
+    <PageLayout title="Disponibilidade">
+      <Header variant="availability" />
+      <div className="px-8">
+        <Divider.Root />
+      </div>
+
+      <div className="p-8">
+        <AvailabilityList />
+      </div>
     </PageLayout>
   );
 } 
