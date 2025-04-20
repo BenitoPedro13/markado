617d8b7686f891eac428f757bc1fe9f5b4dca700
Benito Pedro <benitopedro21@outlook.com>
2025-04-20 07:59:34 -0300

    ✨ REFACTOR: Simplify SignUpLayout by removing the Layout component and directly rendering children within the SignUpProvider. This change streamlines the sign-up flow and enhances code clarity by eliminating unnecessary nesting #141

--- src/app/(auth)/sign-up/layout.tsx
+++ src/app/(auth)/sign-up/layout.tsx
@@ -1,41 +1,16 @@
 'use client';
 
-import { Root as Button } from '@/components/align-ui/ui/button';
-import { SignUpProvider, useSignUp } from '@/contexts/SignUpContext';
-import { RiArrowLeftSLine } from '@remixicon/react';
+import { SignUpProvider } from '@/contexts/SignUpContext';
 import { PropsWithChildren } from 'react';
 import { inferRouterOutputs } from '@trpc/server';
 import { AppRouter } from '~/trpc/server';
 
 type MeResponse = inferRouterOutputs<AppRouter>['user']['me'];
 
-
-const Layout = ({children}: PropsWithChildren) => {
-  const {
-    step,
-    backStep,
-  } = useSignUp();
-
-  
-  return (
-    <>
-      <div className="absolute left-24 top-8">
-        {step !== '/sign-up/email' && (
-          <Button variant="neutral" mode="stroke" onClick={backStep}>
-            <RiArrowLeftSLine size={20} color="var(--text-sub-600)" />
-            <span className="text-text-sub-600">Voltar</span>
-          </Button>
-        )}
-      </div>
-      {children}
-    </>
-  );
-};
-
 export default function SignUpLayout({children, initialUser}: PropsWithChildren & {initialUser: MeResponse | null}) {
   return (
     <SignUpProvider initialUser={initialUser}>
-      <Layout>{children}</Layout>
+      {children}
     </SignUpProvider>
   );
 }
