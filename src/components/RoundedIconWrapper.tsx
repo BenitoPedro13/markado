802eb8a757712ccc5b2b6a77a436d86cc28c57ee
Benito Pedro <benitopedro21@outlook.com>
2025-04-20 08:00:17 -0300

    ✨ CHORE: Adjust margin in RoundedIconWrapper component to improve layout consistency. Changed bottom margin from mb-8 to mb-2 for better spacing in the UI. #141

--- src/components/RoundedIconWrapper.tsx
+++ src/components/RoundedIconWrapper.tsx
@@ -6,7 +6,7 @@ const RoundedIconWrapper = ({children}: PropsWithChildren) => {
 
   // TODO: Background gradient effect
   return (
-    <div className="p-4 rounded-icon-wrapper w-24 h-24 flex justify-center items-center mb-8">
+    <div className="p-4 rounded-icon-wrapper w-24 h-24 flex justify-center items-center mb-2">
       <div className="w-16 h-16 flex justify-center items-center rounded-full border border-bg-soft-200">
         {children}
       </div>
