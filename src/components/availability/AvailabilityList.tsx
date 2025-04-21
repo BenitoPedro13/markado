5f9442c6638525663eb6d9988d40518f3302a20b
marcaumdesign <navalservice1@gmail.com>
2025-04-21 12:38:19 -0300

    ✨ FEAT: Atualiza o componente AvailabilityList para incluir um layout aprimorado com divisores entre as disponibilidades, melhorando a apresentação visual da lista, closes #147

--- src/components/availability/AvailabilityList.tsx
+++ src/components/availability/AvailabilityList.tsx
@@ -3,19 +3,24 @@
 import React from 'react';
 import Availability from './Availability';
 import { availabilities } from '@/data/availability';
-
+import * as Divider from '@/components/align-ui/ui/divider';
 export default function AvailabilityList() {
   return (
-    <div className="flex flex-col">
-      {availabilities.map((availability) => (
-        <Availability
-          key={availability.id}
-          title={availability.title}
-          schedule={availability.schedule}
-          timezone={availability.timezone}
-          isDefault={availability.isDefault}
-        />
-      ))}
-    </div>
+    
+      <div className="rounded-lg w-full border border-stroke-soft-200">
+        {availabilities.map((availability) => (
+          <div key={availability.id}>
+            <Availability
+            key={availability.id}
+            title={availability.title}
+            schedule={availability.schedule}
+            timezone={availability.timezone}
+            isDefault={availability.isDefault}
+          />
+            <Divider.Root />
+          </div>
+        ))}
+      </div>
+    
   );
 } 
