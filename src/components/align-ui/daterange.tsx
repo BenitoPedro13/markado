69fb58fadf2ef478ad435fb34eeb9ccbb42f8100
Benito Pedro <benitopedro21@outlook.com>
2025-04-21 10:58:30 -0300

    ✨ CHORE: Remove deprecated calendar components and Jest configuration. Updated TypeScript settings to target ES2017 and modified module paths for improved compatibility. This cleanup enhances project maintainability and prepares for future feature development. #4

--- src/components/align-ui/daterange.tsx
+++ src/components/align-ui/daterange.tsx
@@ -1,24 +1,24 @@
 'use client';
 
 import * as React from 'react';
-import { format } from 'date-fns';
-import { DateRange } from 'react-day-picker';
+import {format} from 'date-fns';
+import {DateRange} from 'react-day-picker';
 
 import * as Button from '@/components/align-ui/ui/button';
 import * as DatepickerPrimivites from '@/components/align-ui/ui/datepicker';
 import * as Popover from '@/components/align-ui/ui/popover';
-import { RiCalendarLine } from '@remixicon/react';
+import {RiCalendarLine} from '@remixicon/react';
 
-interface DatepickerProps {
+type SingleDatepickerProps = {
+  defaultValue?: DateRange;
   value?: DateRange;
   onChange?: (range: DateRange | undefined) => void;
-  defaultValue?: DateRange;
-}
+};
 
-export function Datepicker({ value, defaultValue, onChange }: DatepickerProps) {
+function Datepicker({value, defaultValue, onChange}: SingleDatepickerProps) {
   const [open, setOpen] = React.useState(false);
   const [range, setRange] = React.useState<DateRange | undefined>(
-    value ?? defaultValue ?? undefined,
+    value ?? defaultValue ?? undefined
   );
 
   const handleChange = (value: DateRange | undefined) => {
@@ -29,21 +29,23 @@ export function Datepicker({ value, defaultValue, onChange }: DatepickerProps) {
   return (
     <Popover.Root open={open} onOpenChange={setOpen}>
       <Popover.Trigger asChild>
-        <Button.Root variant='neutral' mode='stroke'>
-          <Button.Icon><RiCalendarLine className='text-bg-strong-950'/></Button.Icon>
+        <Button.Root variant="neutral" mode="stroke">
+          <Button.Icon>
+            <RiCalendarLine className="text-bg-strong-950" />
+          </Button.Icon>
           {range?.from ? (
             <>
               {format(range.from, 'LLL dd, y')}
               {range.to && <> - {format(range.to, 'LLL dd, y')}</>}
             </>
           ) : (
-            <span>Select date range</span>
+            <span>Selecione um período</span>
           )}
         </Button.Root>
       </Popover.Trigger>
-      <Popover.Content className='p-0' showArrow={false}>
+      <Popover.Content className="p-0" showArrow={false}>
         <DatepickerPrimivites.Calendar
-          mode='range'
+          mode="range"
           selected={range}
           onSelect={handleChange}
         />
