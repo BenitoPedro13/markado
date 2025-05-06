a5a3f25ccb8f95a9a337b9dd86a1109521ebaf1d
Benito Pedro <benitopedro21@outlook.com>
2025-05-06 11:54:06 -0300

    🔧 UPDATE: Modify AvailabilityPage to include user authentication and fetch availability data based on user ID, redirecting unauthenticated users to the sign-in page.

--- src/app/availability/page.tsx
+++ src/app/availability/page.tsx
@@ -4,9 +4,25 @@ import PageLayout from '@/components/PageLayout';
 import Header from '@/components/navigation/Header';
 import * as Divider from '@/components/align-ui/ui/divider';
 import AvailabilityList from '@/components/availability/AvailabilityList';
+import { redirect } from 'next/navigation';
+import { auth } from '@/auth';
+import {getAllAvailabilitiesByUserId} from '~/trpc/server/handlers/availability.handler';
 
 /** Availability page of the website. */
-export default function AvailabilityPage() {
+export default async function AvailabilityPage() {
+
+  const session = await auth();
+
+  const userId = session?.user?.id;
+
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+  const allAvailability = await getAllAvailabilitiesByUserId(userId);
+
+  console.log('[AvailabilityPage] allAvailability', allAvailability);
+
   return (
     <PageLayout title="Disponibilidade">
       <Header variant="availability" />
@@ -15,7 +31,7 @@ export default function AvailabilityPage() {
       </div>
 
       <div className="p-8">
-        <AvailabilityList />
+        <AvailabilityList allAvailability={allAvailability} />
       </div>
     </PageLayout>
   );
