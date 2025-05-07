a8ab8baea38719eecdac0fa1b7d48b5d77fae9b2
Benito Pedro <benitopedro21@outlook.com>
2025-05-07 17:40:38 -0300

    🔧 UPDATE: Refactor AvailabilityDetails component to utilize detailed schedule fetching and form management, enhancing the handling of availability data and improving user experience with dynamic schedule updates.

--- src/components/availability/AvailabilityDetails.tsx
+++ src/components/availability/AvailabilityDetails.tsx
@@ -5,39 +5,96 @@ import * as Switch from '@/components/align-ui/ui/switch';
 import * as Input from '@/components/align-ui/ui/input';
 import * as Button from '@/components/align-ui/ui/button';
 import * as Select from '@/components/align-ui/ui/select';
-import { availabilities, DaySchedule } from '@/data/availability';
-import { RiDeleteBinLine, RiAddLine } from '@remixicon/react';
+import {availabilities, DaySchedule} from '@/data/availability';
+import {RiDeleteBinLine, RiAddLine} from '@remixicon/react';
 import * as Divider from '@/components/align-ui/ui/divider';
+
+import {AppRouter} from '~/trpc/server';
+import {inferRouterOutputs} from '@trpc/server';
+import {useRouter} from 'next/navigation';
+import {FormProvider, useForm} from 'react-hook-form';
+import {SignUpScheduleFormData} from '@/contexts/SignUpContext';
+import {DEFAULT_SCHEDULE} from '@/lib/availability';
+import type {Schedule as ScheduleType, TimeRange} from '@/types/scheadule';
+import Schedule from '../schedules/components/Schedule';
+import {useTRPC} from '@/utils/trpc';
+import {useQuery} from '@tanstack/react-query';
+
+type AvailabilityById =
+  inferRouterOutputs<AppRouter>['availability']['findDetailedScheduleById'];
+
 type AvailabilityDetailsProps = {
   title: string;
+  availability: AvailabilityById;
 };
 
-export default function AvailabilityDetails({title}: AvailabilityDetailsProps) {
-  const [schedules, setSchedules] = useState<Record<string, DaySchedule>>({
-    'Segunda-feira': { enabled: true, startTime: '09:00', endTime: '17:00' },
-    'Terça-feira': { enabled: true, startTime: '09:00', endTime: '17:00' },
-    'Quarta-feira': { enabled: true, startTime: '09:00', endTime: '17:00' },
-    'Quinta-feira': { enabled: true, startTime: '09:00', endTime: '17:00' },
-    'Sexta-feira': { enabled: true, startTime: '09:00', endTime: '17:00' },
-    'Sábado': { enabled: false, startTime: '09:00', endTime: '17:00' },
-    'Domingo': { enabled: false, startTime: '09:00', endTime: '17:00' }
+export type AvailabilityFormValues = {
+  name: string;
+  schedule: ScheduleType;
+  dateOverrides: {ranges: TimeRange[]}[];
+  timeZone: string;
+  isDefault: boolean;
+};
+
+export default function AvailabilityDetails({
+  title,
+  availability
+}: AvailabilityDetailsProps) {
+  const router = useRouter();
+  const trpc = useTRPC();
+
+  const {data: scheduleData, isPending: isFetchingPending} = useQuery(
+    trpc.availability.findDetailedScheduleById.queryOptions(
+      {scheduleId: availability.id, timeZone: availability.timeZone},
+      {
+        enabled: !!availability.id && !availability
+      }
+    )
+  );
+
+  const schedule = availability ?? scheduleData;
+
+  // const [schedules, setSchedules] = useState<Record<string, DaySchedule>>({
+  //   'Segunda-feira': {enabled: true, startTime: '09:00', endTime: '17:00'},
+  //   'Terça-feira': {enabled: true, startTime: '09:00', endTime: '17:00'},
+  //   'Quarta-feira': {enabled: true, startTime: '09:00', endTime: '17:00'},
+  //   'Quinta-feira': {enabled: true, startTime: '09:00', endTime: '17:00'},
+  //   'Sexta-feira': {enabled: true, startTime: '09:00', endTime: '17:00'},
+  //   Sábado: {enabled: false, startTime: '09:00', endTime: '17:00'},
+  //   Domingo: {enabled: false, startTime: '09:00', endTime: '17:00'}
+  // });
+
+  // useEffect(() => {
+  //   const availability = availabilities.find(
+  //     (a) => a.title.toLowerCase() === title.toLowerCase()
+  //   );
+
+  //   if (availability) {
+  //     setSchedules(availability.schedules);
+  //   }
+  // }, [title]);
+
+  const form = useForm<AvailabilityFormValues>({
+    defaultValues: {
+      ...schedule,
+      schedule: schedule.availability || []
+    }
   });
 
-  useEffect(() => {
-    const availability = availabilities.find(a => 
-      a.title.toLowerCase() === title.toLowerCase()
-    );
-    
-    if (availability) {
-      setSchedules(availability.schedules);
-    }
-  }, [title]);
+  if (!availability) {
+    router.back();
+    return null;
+  }
+
+  // if (isFetchingPending) return null;
+
+  console.log('schedules', schedule);
 
   return (
     <form className="space-y-6">
       <div className="space-y-4">
         <div className="text-title-h6">Horários</div>
-        <div className="space-y-4">
+        {/* <div className="space-y-4">
           {Object.entries(schedules).map(([day, schedule]) => (
             <div key={day} className="flex items-center gap-4">
               <div className="flex items-center gap-2">
@@ -46,7 +103,7 @@ export default function AvailabilityDetails({title}: AvailabilityDetailsProps) {
                   onCheckedChange={(checked) =>
                     setSchedules((prev) => ({
                       ...prev,
-                      [day]: { ...prev[day], enabled: checked }
+                      [day]: {...prev[day], enabled: checked}
                     }))
                   }
                 />
@@ -59,7 +116,7 @@ export default function AvailabilityDetails({title}: AvailabilityDetailsProps) {
                   onChange={(e) =>
                     setSchedules((prev) => ({
                       ...prev,
-                      [day]: { ...prev[day], startTime: e.target.value }
+                      [day]: {...prev[day], startTime: e.target.value}
                     }))
                   }
                   disabled={!schedule.enabled}
@@ -72,7 +129,7 @@ export default function AvailabilityDetails({title}: AvailabilityDetailsProps) {
                   onChange={(e) =>
                     setSchedules((prev) => ({
                       ...prev,
-                      [day]: { ...prev[day], endTime: e.target.value }
+                      [day]: {...prev[day], endTime: e.target.value}
                     }))
                   }
                   disabled={!schedule.enabled}
@@ -86,10 +143,13 @@ export default function AvailabilityDetails({title}: AvailabilityDetailsProps) {
               </Button.Root>
             </div>
           ))}
-        </div>
+        </div> */}
+        <FormProvider {...form}>
+          <Schedule control={form.control} name="schedule" weekStart={1} />
+        </FormProvider>
       </div>
 
-<Divider.Root />
+      <Divider.Root />
 
       <div className="space-y-4">
         <div className="text-title-h6">Avançado</div>
@@ -109,4 +169,4 @@ export default function AvailabilityDetails({title}: AvailabilityDetailsProps) {
       </div>
     </form>
   );
-} 
+}
