5d3a328f6543b180e660172cbcdd22583ca1c411
Thiago Arruda <thiago_arruda@id.uff.br>
2025-05-12 14:14:04 -0300

    📦 NEW: Finalization form

---
+++ src/app/[username]/[service]/[status]/page.tsx
@@ -0,0 +1,7 @@
+import ServiceFinalizationForm from '@/modules/scheduling/services/ServiceFinalizationForm';
+
+const ServiceSchedulingFinalizationPage = () => {
+  return <ServiceFinalizationForm />;
+};
+
+export default ServiceSchedulingFinalizationPage;
---
+++ src/modules/scheduling/services/ServiceFinalizationForm.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import * as Divider from '@/components/align-ui/ui/divider';
+import { useScheduling } from '@/contexts/SchedulingContext';
+import Image from 'next/image';
+import { useParams } from 'next/navigation';
+const ServiceFinalizationForm = () => {
+  const {status} = useParams();
+  const {service} = useScheduling();
+
+  return (
+    <div className="min-h-screen w-full flex flex-col items-center justify-center">
+      <div className="w-full md:max-w-[400px] flex flex-col gap-5 border border-stroke-soft-200 rounded-3xl p-6">
+        <div className="flex flex-col gap-1">
+          <Image src="/images/logo.svg" alt="Logo" width={100} height={100} />
+          <h1 className="text-label-xl font-medium text-center">
+            Reunião marcada com sucesso!
+          </h1>
+          <p className="text-paragraph-lg text-text-strong-950 text-center">
+            Um e-mail com detalhes sobre a reunião foi enviado para todos os
+            participantes.
+          </p>
+        </div>
+        <Divider.Root className="w-full" />
+        <div className="flex justify-between items-center">
+          <p className="text-paragraph-lg text-text-strong-950">
+            Assunto:
+          </p>
+          <p className="text-label-sm font-medium text-text-strong-950">
+            {service}
+          </p>
+        </div>
+        <Divider.Root className="w-full" />
+        <Link href="/">
+        Precisa alterar?
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceFinalizationForm;
