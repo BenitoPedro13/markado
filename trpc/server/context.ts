a73760d338b07fb986a1cd23f4f64b4bd42b9136
Benito Pedro <benitopedro21@outlook.com>
2025-05-13 14:14:48 -0300

    ✨ FEAT: Enhance createContext function to include user retrieval and session management, improving user context handling in tRPC server.

--- trpc/server/context.ts
+++ trpc/server/context.ts
@@ -1,15 +1,38 @@
-import { auth } from '@/auth';
-import { prisma } from '@/lib/prisma';
-import { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
+import {auth} from '@/auth';
+import {prisma} from '@/lib/prisma';
+import {FetchCreateContextFnOptions} from '@trpc/server/adapters/fetch';
+import {getMeByUserId} from './handlers/user.handler';
+import {Me} from '@/app/settings/page';
+import {Session} from 'next-auth';
+import type {PrismaClient} from '~/prisma/app/generated/prisma/client';
 
-export async function createContext(opts: FetchCreateContextFnOptions) {
+export async function createContext(
+  opts: FetchCreateContextFnOptions
+): Promise<{
+  user: Me | null;
+  session: Session | null;
+  prisma: PrismaClient;
+  req: Request;
+}> {
   const session = await auth();
-  
+
+  if (session?.user.id) {
+    const user = await getMeByUserId(session.user.id);
+
+    return {
+      user,
+      session,
+      prisma,
+      req: opts.req
+    };
+  }
+
   return {
+    user: null,
     session,
     prisma,
-    req: opts.req,
+    req: opts.req
   };
 }
 
-export type Context = Awaited<ReturnType<typeof createContext>>; 
+export type Context = Awaited<ReturnType<typeof createContext>>;
