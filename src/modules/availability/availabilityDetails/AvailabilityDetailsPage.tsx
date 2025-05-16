f58b7c6ff5d55bf52e1a50664cc6543f26fb7010
Benito Pedro <benitopedro21@outlook.com>
2025-05-16 07:40:49 -0300

    🔧 REFACTOR: Update AvailabilityDetailsPage to render AvailabilityDetailsHeader and AvailabilityDetails components

--- src/modules/availability/availabilityDetails/AvailabilityDetailsPage.tsx
+++ src/modules/availability/availabilityDetails/AvailabilityDetailsPage.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import AvailabilityDetailsHeader from '@/components/availability/AvailabilityDetailsHeader';
+import React from 'react';
+
+import * as Divider from '@/components/align-ui/ui/divider';
+import AvailabilityDetails from '@/components/availability/AvailabilityDetails';
 
 export default function AvailabilityDetailsPage() {
   return (
-    <div>AvailabilityDetailsPage</div>
-  )
+    <>
+      <AvailabilityDetailsHeader />
+      <div className="px-8">
+        <Divider.Root />
+      </div>
+
+      <div className="p-8">
+        <AvailabilityDetails />
+      </div>
+    </>
+  );
 }
