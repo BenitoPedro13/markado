2df5a03e7c6173b0278e1fd9d2ca0a011ea9b1bc
Benito Pedro <benitopedro21@outlook.com>
2025-05-16 09:21:39 -0300

    🔧 REFACTOR: Enhance delete schedule functionality with notification feedback and improved modal structure

--- src/components/availability/Availability.tsx
+++ src/components/availability/Availability.tsx
@@ -15,6 +15,10 @@ import * as Dropdown from '@/components/align-ui/ui/dropdown';
 import * as Modal from '@/components/align-ui/ui/modal';
 import * as Button from '@/components/align-ui/ui/button';
 import Link from 'next/link';
+import {submitDeleteSchedule} from '~/trpc/server/handlers/schedule.handler';
+import {useRouter} from 'next/navigation';
+import {useNotification} from '@/hooks/use-notification';
+import { useLocale } from '@/hooks/use-locale';
 
 type AvailabilityProps = {
   id: number;
@@ -35,7 +39,13 @@ export default function Availability({
   onDuplicate,
   onDelete
 }: AvailabilityProps) {
+  const {notification} = useNotification();
+
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  const router = useRouter();
+
+  const {t} = useLocale('Availability');
 
   const handleDelete = () => {
     onDelete(id);
@@ -108,39 +118,66 @@ export default function Availability({
 
       <Modal.Root open={isDeleteModalOpen} onOpenChange={setIsDeleteModalOpen}>
         <Modal.Content className="max-w-[440px]">
-          <Modal.Body className="flex items-start gap-4">
-            <div className="rounded-10 bg-error-lighter flex size-10 shrink-0 items-center justify-center">
-              <RiDeleteBinLine className="text-error-base size-6" />
-            </div>
-            <div className="space-y-1">
-              <div className="text-label-md text-text-strong-950">
-                Apagar "{title}"
+          <form
+            action={async (formData) => {
+              try {
+                // console.log('availability.id', id);
+                await submitDeleteSchedule(id);
+
+                notification({
+                  title: 'Alterações salvas!',
+                  description: 'Seus updates foram salvos com sucesso.',
+                  variant: 'stroke',
+                  status: 'success'
+                });
+
+                // router.push(`/availability`);
+              } catch (error: any) {
+                console.error('Error submitting availability form:', error);
+                notification({
+                  title: t('schedule_updated_error'),
+                  description: error.message,
+                  variant: 'stroke',
+                  id: 'schedule_updated_error',
+                  status: 'error'
+                });
+              }
+            }}
+          >
+            <Modal.Body className="flex items-start gap-4">
+              <div className="rounded-10 bg-error-lighter flex size-10 shrink-0 items-center justify-center">
+                <RiDeleteBinLine className="text-error-base size-6" />
               </div>
-              <div className="text-paragraph-sm text-text-sub-600">
-                Você não poderá recuperar a disponibilidade após apagá-la.
+              <div className="space-y-1">
+                <div className="text-label-md text-text-strong-950">
+                  Apagar "{title}"
+                </div>
+                <div className="text-paragraph-sm text-text-sub-600">
+                  Você não poderá recuperar a disponibilidade após apagá-la.
+                </div>
               </div>
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <Modal.Close asChild>
+            </Modal.Body>
+            <Modal.Footer>
+              <Modal.Close asChild>
+                <Button.Root
+                  variant="neutral"
+                  mode="stroke"
+                  size="small"
+                  className="w-full"
+                >
+                  Cancelar
+                </Button.Root>
+              </Modal.Close>
               <Button.Root
-                variant="neutral"
-                mode="stroke"
+                variant="error"
                 size="small"
                 className="w-full"
+                // onClick={handleDelete}
               >
-                Cancelar
+                Apagar
               </Button.Root>
-            </Modal.Close>
-            <Button.Root
-              variant="error"
-              size="small"
-              className="w-full"
-              onClick={handleDelete}
-            >
-              Apagar
-            </Button.Root>
-          </Modal.Footer>
+            </Modal.Footer>
+          </form>
         </Modal.Content>
       </Modal.Root>
     </>
