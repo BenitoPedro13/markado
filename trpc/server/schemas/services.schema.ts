c62ec14453bc4fd9b55ddaa40c8ffb5566a9664f
Benito Pedro <benitopedro21@outlook.com>
2025-05-20 20:03:30 -0300

    Add new schemas for event type filtering and retrieval, including strict validation for query parameters and input structures using Zod

--- trpc/server/schemas/services.schema.ts
+++ trpc/server/schemas/services.schema.ts
@@ -47,3 +47,38 @@ export type EventTypeLocation = z.infer<
 export const ZCreateInputSchema = createEventTypeInput;
 
 export type TCreateInputSchema = z.infer<typeof ZCreateInputSchema>;
+
+
+// get all by viewer
+
+export const filterQuerySchemaStrict = z.object({
+  teamIds: z.number().array().optional(),
+  // A user can only filter by only his userId
+  upIds: z.string().array().max(1).optional(),
+  schedulingTypes: z.nativeEnum(SchedulingType).array().optional()
+});
+
+export const ZEventTypeInputSchema = z
+  .object({
+    filters: filterQuerySchemaStrict.optional(),
+    forRoutingForms: z.boolean().optional()
+  })
+  .nullish();
+
+export type TEventTypeInputSchema = z.infer<typeof ZEventTypeInputSchema>;
+
+export const ZGetEventTypesFromGroupSchema = z.object({
+  filters: filterQuerySchemaStrict.optional(),
+  forRoutingForms: z.boolean().optional(),
+  cursor: z.number().nullish(),
+  limit: z.number().default(10),
+  group: z.object({
+    teamId: z.number().nullish(),
+    parentId: z.number().nullish()
+  }),
+  searchQuery: z.string().optional()
+});
+
+export type TGetEventTypesFromGroupSchema = z.infer<
+  typeof ZGetEventTypesFromGroupSchema
+>;
