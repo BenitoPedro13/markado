9a5fe9bb513bf0ca18f71b3bd057e30b47ed2c39
Benito Pedro <benitopedro21@outlook.com>
2025-05-21 10:30:45 -0300

    Refactor AvailabilityListPage component by removing unused imports, enhancing code clarity and maintainability.

--- src/modules/availability/AvailabilityListPage.tsx
+++ src/modules/availability/AvailabilityListPage.tsx
@@ -1,20 +1,11 @@
 'use client';
 
 import AvailabilityHeader from '@/components/availability/AvailabilityHeader';
-import React, {useRef} from 'react';
+import React from 'react';
 
 import * as Divider from '@/components/align-ui/ui/divider';
 import AvailabilityList from '@/components/availability/AvailabilityList';
 import {useAvailability} from '@/contexts/availability/AvailabilityContext';
-import type {
-  TAvailabilityList,
-  TAvailabilityListItem
-} from '@/contexts/availability/AvailabilityContext';
-import {submitCreateSchedule} from '~/trpc/server/handlers/schedule.handler';
-import {notification} from '@/hooks/use-notification';
-import {useLocale} from '@/hooks/use-locale';
-import {useOptimistic} from 'react';
-import {TFormatedAvailabilitiesBySchedule} from '@/utils/formatAvailability';
 
 export default function AvailabilityListPage() {
   const {
