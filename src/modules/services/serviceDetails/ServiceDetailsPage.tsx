f6ba3dbcc0ee47854c331e7761a2fcd3e43db6d4
Benito Pedro <benitopedro21@outlook.com>
2025-05-26 16:22:53 -0300

    ✨ ADD: Create ServiceDetailsPage component to display service configuration with header and sidebar. #35

---
+++ src/modules/services/serviceDetails/ServiceDetailsPage.tsx
@@ -0,0 +1,18 @@
+import ServicesDetailsSidebar from '@/components/services/service-details/ServicesDetailsSidebar'
+import ServicesDetailsHeader from '@/components/services/ServicesDetailsHeader'
+import React from 'react'
+import * as Divider from '@/components/align-ui/ui/divider';
+
+function ServiceDetailsPage({slug}: {slug: string}) {
+  return (
+    <>
+      <ServicesDetailsHeader title="Configuração do Serviço" />
+      <div className="px-8">
+        <Divider.Root />
+      </div>
+      <ServicesDetailsSidebar slug={slug} />
+    </>
+  );
+}
+
+export default ServiceDetailsPage;
