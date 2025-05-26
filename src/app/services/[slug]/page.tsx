eab604aac853d9e88dd4f91c7627d5af1d671b81
Benito Pedro <benitopedro21@outlook.com>
2025-05-26 18:22:02 -0300

    ✨ REFACTOR: Integrate ServicesDetailsProvider into ServiceDetailsServerPage for improved state management #35

--- src/app/services/[slug]/page.tsx
+++ src/app/services/[slug]/page.tsx
@@ -1,7 +1,11 @@
 export const dynamic = 'force-dynamic';
+import { auth } from '@/auth';
 import PageLayout from '@/components/PageLayout';
+import {ServicesDetailsProvider} from '@/contexts/services/servicesDetails/ServicesContext';
 
 import ServiceDetailsPage from '@/modules/services/serviceDetails/ServiceDetailsPage';
+import { findDetailedScheduleById } from '~/trpc/server/handlers/availability.handler';
+import { getMeByUserId } from '~/trpc/server/handlers/user.handler';
 
 type Props = {
   params: {
@@ -12,9 +16,30 @@ type Props = {
 export default async function ServiceDetailsServerPage({
   params: {slug}
 }: Props) {
+  const session = await auth();
+
+  const userId = session?.user?.id;
+
+  if (!userId) return;
+
+  const availabilityId = slug;
+
+  // const availability = await findDetailedScheduleById({
+  //   scheduleId: +availabilityId,
+  //   userId: userId
+  // });
+  const me = await getMeByUserId(userId);
+
+  // const title = availability?.name;
+
   return (
     <PageLayout title="Detalhes do Serviço">
-      <ServiceDetailsPage slug={slug} />
+      <ServicesDetailsProvider
+        initialServiceDetails={null}
+        initialMe={me}
+      >
+        <ServiceDetailsPage slug={slug} />
+      </ServicesDetailsProvider>
     </PageLayout>
   );
 }
