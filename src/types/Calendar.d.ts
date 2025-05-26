60a1dbe28d07a2bf5afb454f7ccde8a11e423778
Benito Pedro <benitopedro21@outlook.com>
2025-05-26 19:11:51 -0300

    ✨ ADD: Introduce RecurringEvent interface in Calendar type definitions for event scheduling

---
+++ src/types/Calendar.d.ts
@@ -0,0 +1,10 @@
+import type {Frequency} from '~/prisma/zod-utils';
+
+export interface RecurringEvent {
+  dtstart?: Date | undefined;
+  interval: number;
+  count: number;
+  freq: Frequency;
+  until?: Date | undefined;
+  tzid?: string | undefined;
+}
