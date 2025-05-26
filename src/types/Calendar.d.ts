cc51b2a3447268867396136a35a21d07296c35ad
Benito Pedro <benitopedro21@outlook.com>
2025-05-26 19:33:04 -0300

    ✨ ADD: Define IntervalLimitUnit and IntervalLimit types for interval limit management #35

--- src/types/Calendar.d.ts
+++ src/types/Calendar.d.ts
@@ -1,5 +1,11 @@
 import type {Frequency} from '~/prisma/zod-utils';
 
+export type IntervalLimitUnit = 'day' | 'week' | 'month' | 'year';
+
+export type IntervalLimit = Partial<
+  Record<`PER_${Uppercase<IntervalLimitUnit>}`, number | undefined>
+>;
+
 export interface RecurringEvent {
   dtstart?: Date | undefined;
   interval: number;
