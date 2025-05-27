951808235e75c793d974619b9d90f69b44edadc1
Benito Pedro <benitopedro21@outlook.com>
2025-05-27 15:45:56 -0300

    👌 IMPROVE: exported ChildrenEventType from App.d.ts

--- src/packages/types/App.d.ts
+++ src/packages/types/App.d.ts
@@ -1,5 +1,23 @@
 import type { AppCategories, Prisma } from "~/prisma/app/generated/prisma/client";
 
+export type ChildrenEventType = {
+  value: string;
+  label: string;
+  created: boolean;
+  owner: {
+    avatar: string;
+    id: number;
+    email: string;
+    name: string;
+    username: string;
+    membership: MembershipRole;
+    eventTypeSlugs: string[];
+    profile: UserProfile;
+  };
+  slug: string;
+  hidden: boolean;
+};
+
 type CommonProperties = {
   default?: false;
   type: string;
