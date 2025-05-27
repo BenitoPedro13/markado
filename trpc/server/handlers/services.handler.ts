0d8cf32d39cba1103b103c58e200f7be38db8960
Benito Pedro <benitopedro21@outlook.com>
2025-05-27 07:40:18 -0300

    ✨ CHANGE: Implement updateServiceHandler function for updating event types with enhanced validation and error handling #172

--- trpc/server/handlers/services.handler.ts
+++ trpc/server/handlers/services.handler.ts
@@ -27,6 +27,30 @@ import {hasFilter, mapEventType} from '~/trpc/server/utils/services/util';
 import {revalidatePath} from 'next/cache';
 import getEventTypeBySlug from '@/packages/event-types/getEventTypeBySlug';
 
+// import type {NextApiResponse, GetServerSidePropsContext} from 'next';
+
+// import type {appDataSchemas} from '@/app-store/apps.schemas.generated';
+// import updateChildrenEventTypes from '@/features/ee/managed-event-types/lib/handleChildrenEventTypes';
+// import {
+//   allowDisablingAttendeeConfirmationEmails,
+//   allowDisablingHostConfirmationEmails
+// } from '@/features/ee/workflows/lib/allowDisablingStandardEmails';
+import {validateIntervalLimitOrder} from '@/packages/lib';
+// import {getTranslation} from '@/lib/server';
+import {validateBookerLayouts} from '@/packages/lib/validateBookerLayouts';
+// import {WorkflowTriggerEvents} from '~/prisma/app/generated/prisma/client';
+
+
+// import type {TrpcSessionUser} from '../../../trpc';
+// import {setDestinationCalendarHandler} from '../../loggedInViewer/setDestinationCalendar.handler';
+import {
+  ensureUniqueBookingFields,
+  ensureEmailOrPhoneNumberIsPresent,
+  handleCustomInputs,
+  handlePeriodType
+} from '~/trpc/server/utils/services/util';
+import { TUpdateInputSchema } from '~/trpc/server/schemas/services.schema';
+
 // Create
 
 type CreateOptions = {
@@ -442,6 +466,569 @@ const filterEventTypes = async (
 
   return filteredEventTypes;
 };
+
+// Update
+
+
+type UpdateOptions = {
+  input: TUpdateInputSchema;
+};
+
+export type UpdateEventTypeReturn = Awaited<ReturnType<typeof updateServiceHandler>>;
+
+export const updateServiceHandler = async ({input}: UpdateOptions) => {
+  const session = await auth();
+
+  if (!session) {
+    throw new TRPCError({
+      code: 'UNAUTHORIZED',
+      message: 'updateServiceHandler: Could not get the user session'
+    });
+  }
+
+  if (!session.user) {
+    throw new TRPCError({
+      code: 'UNAUTHORIZED',
+      message: 'updateServiceHandler: Not authenticated'
+    });
+  }
+
+  const user = await UserRepository.findByIdOrThrow({id: session.user.id});
+  const enrichedUser = await UserRepository.enrichUserWithItsProfile({user});
+
+  const userProfile = enrichedUser.profile;
+
+  const {
+    schedule,
+    instantMeetingSchedule,
+    periodType,
+    locations,
+    bookingLimits,
+    durationLimits,
+    destinationCalendar,
+    customInputs,
+    recurringEvent,
+    eventTypeColor,
+    users,
+    children,
+    assignAllTeamMembers,
+    hosts,
+    id,
+    multiplePrivateLinks,
+    // Extract this from the input so it doesn't get saved in the db
+    // eslint-disable-next-line
+    userId,
+    bookingFields,
+    offsetStart,
+    secondaryEmailId,
+    aiPhoneCallConfig,
+    isRRWeightsEnabled,
+    ...rest
+  } = input;
+
+  const eventType = await prisma.eventType.findUniqueOrThrow({
+    where: {id},
+    select: {
+      title: true,
+      // isRRWeightsEnabled: true,
+      hosts: {
+        select: {
+          userId: true,
+          priority: true,
+          weight: true,
+          isFixed: true
+        }
+      },
+      // aiPhoneCallConfig: {
+      //   select: {
+      //     generalPrompt: true,
+      //     beginMessage: true,
+      //     enabled: true,
+      //     llmId: true
+      //   }
+      // },
+      children: {
+        select: {
+          userId: true
+        }
+      },
+      workflows: {
+        select: {
+          workflowId: true
+        }
+      },
+      team: {
+        select: {
+          id: true,
+          name: true,
+          slug: true,
+          parentId: true,
+          parent: {
+            select: {
+              slug: true
+            }
+          },
+          members: {
+            select: {
+              role: true,
+              accepted: true,
+              user: {
+                select: {
+                  name: true,
+                  id: true,
+                  email: true,
+                  eventTypes: {
+                    select: {
+                      slug: true
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  });
+
+  if (
+    input.teamId &&
+    eventType.team?.id &&
+    input.teamId !== eventType.team.id
+  ) {
+    throw new TRPCError({code: 'UNAUTHORIZED'});
+  }
+
+  const teamId = input.teamId || eventType.team?.id;
+
+  ensureUniqueBookingFields(bookingFields);
+  ensureEmailOrPhoneNumberIsPresent(bookingFields);
+
+  const data: Prisma.EventTypeUpdateInput = {
+    ...rest,
+    bookingFields,
+    // isRRWeightsEnabled,
+    metadata:
+      rest.metadata === null
+        ? Prisma.DbNull
+        : (rest.metadata as Prisma.InputJsonObject),
+    eventTypeColor:
+      eventTypeColor === null
+        ? Prisma.DbNull
+        : (eventTypeColor as Prisma.InputJsonObject)
+  };
+  data.locations = locations ?? undefined;
+  if (periodType) {
+    data.periodType = handlePeriodType(periodType);
+  }
+
+  if (recurringEvent) {
+    data.recurringEvent = {
+      dstart: recurringEvent.dtstart as unknown as Prisma.InputJsonObject,
+      interval: recurringEvent.interval,
+      count: recurringEvent.count,
+      freq: recurringEvent.freq,
+      until: recurringEvent.until as unknown as Prisma.InputJsonObject,
+      tzid: recurringEvent.tzid
+    };
+  } else if (recurringEvent === null) {
+    data.recurringEvent = Prisma.DbNull;
+  }
+
+  // if (destinationCalendar) {
+  //   /** We connect or create a destination calendar to the event type instead of the user */
+  //   await setDestinationCalendarHandler({
+  //     ctx,
+  //     input: {
+  //       ...destinationCalendar,
+  //       eventTypeId: id
+  //     }
+  //   });
+  // }
+
+  if (customInputs) {
+    data.customInputs = handleCustomInputs(customInputs, id);
+  }
+
+  if (bookingLimits) {
+    const isValid = validateIntervalLimitOrder(bookingLimits);
+    if (!isValid)
+      throw new TRPCError({
+        code: 'BAD_REQUEST',
+        message: 'Booking limits must be in ascending order.'
+      });
+    data.bookingLimits = bookingLimits;
+  }
+
+  if (durationLimits) {
+    const isValid = validateIntervalLimitOrder(durationLimits);
+    if (!isValid)
+      throw new TRPCError({
+        code: 'BAD_REQUEST',
+        message: 'Duration limits must be in ascending order.'
+      });
+    data.durationLimits = durationLimits;
+  }
+
+  if (offsetStart !== undefined) {
+    if (offsetStart < 0) {
+      throw new TRPCError({
+        code: 'BAD_REQUEST',
+        message: 'Offset start time must be zero or greater.'
+      });
+    }
+    data.offsetStart = offsetStart;
+  }
+
+  const bookerLayoutsError = validateBookerLayouts(
+    input.metadata?.bookerLayouts || null
+  );
+  if (bookerLayoutsError) {
+    throw new TRPCError({
+      code: 'BAD_REQUEST',
+      message: 'bookerLayoutsError: ' + bookerLayoutsError
+    });
+  }
+
+  if (schedule) {
+    // Check that the schedule belongs to the user
+    const userScheduleQuery = await prisma.schedule.findFirst({
+      where: {
+        userId: user.id,
+        id: schedule
+      }
+    });
+    if (userScheduleQuery) {
+      data.schedule = {
+        connect: {
+          id: schedule
+        }
+      };
+    }
+  }
+  // allows unsetting a schedule through { schedule: null, ... }
+  else if (null === schedule) {
+    data.schedule = {
+      disconnect: true
+    };
+  }
+
+  if (instantMeetingSchedule) {
+    data.instantMeetingSchedule = {
+      connect: {
+        id: instantMeetingSchedule
+      }
+    };
+  } else if (schedule === null) {
+    data.instantMeetingSchedule = {
+      disconnect: true
+    };
+  }
+
+  if (users?.length) {
+    data.users = {
+      set: [],
+      connect: users.map((userId: string) => ({id: userId}))
+    };
+  }
+
+  if (teamId && hosts) {
+    // check if all hosts can be assigned (memberships that have accepted invite)
+    const memberships =
+      (await prisma.membership.findMany({
+        where: {
+          teamId,
+          accepted: true
+        }
+      })) || [];
+    const teamMemberIds = memberships.map((membership) => membership.userId);
+    // guard against missing IDs, this may mean a member has just been removed
+    // or this request was forged.
+    // we let this pass through on organization sub-teams
+    if (
+      !hosts.every((host) => teamMemberIds.includes(host.userId)) &&
+      !eventType.team?.parentId
+    ) {
+      throw new TRPCError({
+        code: 'FORBIDDEN'
+      });
+    }
+
+    // // weights were already enabled or are enabled now
+    // const isWeightsEnabled =
+    //   isRRWeightsEnabled ||
+    //   (typeof isRRWeightsEnabled === 'undefined' &&
+    //     eventType.isRRWeightsEnabled);
+
+    const oldHostsSet = new Set(
+      eventType.hosts.map((oldHost) => oldHost.userId)
+    );
+    const newHostsSet = new Set(hosts.map((oldHost) => oldHost.userId));
+
+    const existingHosts = hosts.filter((newHost) =>
+      oldHostsSet.has(newHost.userId)
+    );
+    const newHosts = hosts.filter(
+      (newHost) => !oldHostsSet.has(newHost.userId)
+    );
+    const removedHosts = eventType.hosts.filter(
+      (oldHost) => !newHostsSet.has(oldHost.userId)
+    );
+
+    data.hosts = {
+      deleteMany: {
+        OR: removedHosts.map((host) => ({
+          userId: host.userId,
+          eventTypeId: id
+        }))
+      },
+      create: newHosts.map((host) => {
+        return {
+          ...host,
+          isFixed:
+            data.schedulingType === SchedulingType.COLLECTIVE || host.isFixed,
+          priority: host.priority ?? 2,
+          weight: host.weight ?? 100
+        };
+      }),
+      update: existingHosts.map((host) => ({
+        where: {
+          userId_eventTypeId: {
+            userId: host.userId,
+            eventTypeId: id
+          }
+        },
+        data: {
+          isFixed:
+            data.schedulingType === SchedulingType.COLLECTIVE || host.isFixed,
+          priority: host.priority ?? 2,
+          weight: host.weight ?? 100
+        }
+      }))
+    };
+  }
+
+  if (input.metadata?.disableStandardEmails?.all) {
+    if (!eventType?.team?.parentId) {
+      input.metadata.disableStandardEmails.all.host = false;
+      input.metadata.disableStandardEmails.all.attendee = false;
+    }
+  }
+
+  // if (input.metadata?.disableStandardEmails?.confirmation) {
+  //   //check if user is allowed to disabled standard emails
+  //   const workflows = await ctx.prisma.workflow.findMany({
+  //     where: {
+  //       activeOn: {
+  //         some: {
+  //           eventTypeId: input.id
+  //         }
+  //       },
+  //       trigger: WorkflowTriggerEvents.NEW_EVENT
+  //     },
+  //     include: {
+  //       steps: true
+  //     }
+  //   });
+
+  //   if (input.metadata?.disableStandardEmails.confirmation?.host) {
+  //     if (!allowDisablingHostConfirmationEmails(workflows)) {
+  //       input.metadata.disableStandardEmails.confirmation.host = false;
+  //     }
+  //   }
+
+  //   if (input.metadata?.disableStandardEmails.confirmation?.attendee) {
+  //     if (!allowDisablingAttendeeConfirmationEmails(workflows)) {
+  //       input.metadata.disableStandardEmails.confirmation.attendee = false;
+  //     }
+  //   }
+  // }
+
+  // for (const appKey in input.metadata?.apps) {
+  //   const app = input.metadata?.apps[appKey as keyof typeof appDataSchemas];
+  //   // There should only be one enabled payment app in the metadata
+  //   if (app.enabled && app.price && app.currency) {
+  //     data.price = app.price;
+  //     data.currency = app.currency;
+  //     break;
+  //   }
+  // }
+  const connectedLinks = await prisma.hashedLink.findMany({
+    where: {
+      eventTypeId: input.id
+    },
+    select: {
+      id: true,
+      link: true
+    }
+  });
+
+  const connectedMultiplePrivateLinks = connectedLinks.map((link) => link.link);
+
+  if (multiplePrivateLinks && multiplePrivateLinks.length > 0) {
+    const multiplePrivateLinksToBeInserted = multiplePrivateLinks.filter(
+      (link) => !connectedMultiplePrivateLinks.includes(link)
+    );
+    const singleLinksToBeDeleted = connectedMultiplePrivateLinks.filter(
+      (link) => !multiplePrivateLinks.includes(link)
+    );
+    if (singleLinksToBeDeleted.length > 0) {
+      await prisma.hashedLink.deleteMany({
+        where: {
+          eventTypeId: input.id,
+          link: {
+            in: singleLinksToBeDeleted
+          }
+        }
+      });
+    }
+    if (multiplePrivateLinksToBeInserted.length > 0) {
+      await prisma.hashedLink.createMany({
+        data: multiplePrivateLinksToBeInserted.map((link) => {
+          return {
+            link: link,
+            eventTypeId: input.id
+          };
+        })
+      });
+    }
+  } else {
+    // Delete all the single-use links for this event.
+    if (connectedMultiplePrivateLinks.length > 0) {
+      await prisma.hashedLink.deleteMany({
+        where: {
+          eventTypeId: input.id,
+          link: {
+            in: connectedMultiplePrivateLinks
+          }
+        }
+      });
+    }
+  }
+
+  if (assignAllTeamMembers !== undefined) {
+    data.assignAllTeamMembers = assignAllTeamMembers;
+  }
+
+  // Validate the secondary email
+  if (secondaryEmailId) {
+    const secondaryEmail = await prisma.secondaryEmail.findUnique({
+      where: {
+        id: secondaryEmailId,
+        userId: user.id
+      }
+    });
+    // Make sure the secondary email id belongs to the current user and its a verified one
+    if (secondaryEmail && secondaryEmail.emailVerified) {
+      data.secondaryEmail = {
+        connect: {
+          id: secondaryEmailId
+        }
+      };
+      // Delete the data if the user selected his original email to send the events to, which means the value coming will be -1
+    } else if (secondaryEmailId === -1) {
+      data.secondaryEmail = {
+        disconnect: true
+      };
+    }
+  }
+
+  // if (aiPhoneCallConfig) {
+  //   if (aiPhoneCallConfig.enabled) {
+  //     await ctx.prisma.aIPhoneCallConfiguration.upsert({
+  //       where: {
+  //         eventTypeId: id
+  //       },
+  //       update: {
+  //         ...aiPhoneCallConfig,
+  //         guestEmail: !!aiPhoneCallConfig?.guestEmail
+  //           ? aiPhoneCallConfig.guestEmail
+  //           : null,
+  //         guestCompany: !!aiPhoneCallConfig?.guestCompany
+  //           ? aiPhoneCallConfig.guestCompany
+  //           : null
+  //       },
+  //       create: {
+  //         ...aiPhoneCallConfig,
+  //         guestEmail: !!aiPhoneCallConfig?.guestEmail
+  //           ? aiPhoneCallConfig.guestEmail
+  //           : null,
+  //         guestCompany: !!aiPhoneCallConfig?.guestCompany
+  //           ? aiPhoneCallConfig.guestCompany
+  //           : null,
+  //         eventTypeId: id
+  //       }
+  //     });
+  //   } else if (!aiPhoneCallConfig.enabled && eventType.aiPhoneCallConfig) {
+  //     await ctx.prisma.aIPhoneCallConfiguration.delete({
+  //       where: {
+  //         eventTypeId: id
+  //       }
+  //     });
+  //   }
+  // }
+
+  const updatedEventTypeSelect = Prisma.validator<Prisma.EventTypeSelect>()({
+    slug: true,
+    schedulingType: true
+  });
+  let updatedEventType: Prisma.EventTypeGetPayload<{
+    select: typeof updatedEventTypeSelect;
+  }>;
+  try {
+    updatedEventType = await prisma.eventType.update({
+      where: {id},
+      data,
+      select: updatedEventTypeSelect
+    });
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === 'P2002') {
+        // instead of throwing a 500 error, catch the conflict and throw a 400 error.
+        throw new TRPCError({
+          message: 'error_event_type_url_duplicate',
+          code: 'BAD_REQUEST'
+        });
+      }
+    }
+    throw e;
+  }
+  const updatedValues = Object.entries(data).reduce((acc, [key, value]) => {
+    if (value !== undefined) {
+      // @ts-expect-error Element implicitly has any type
+      acc[key] = value;
+    }
+    return acc;
+  }, {});
+
+  // Handling updates to children event types (managed events types)
+  // await updateChildrenEventTypes({
+  //   eventTypeId: id,
+  //   currentUserId: ctx.user.id,
+  //   oldEventType: eventType,
+  //   updatedEventType,
+  //   children,
+  //   profileId: ctx.user.profile.id,
+  //   prisma: ctx.prisma,
+  //   updatedValues
+  // });
+
+  // const res = ctx.res as NextApiResponse;
+  // if (typeof res?.revalidate !== 'undefined') {
+  //   try {
+  //     await res?.revalidate(`/${ctx.user.username}/${updatedEventType.slug}`);
+  //   } catch (e) {
+  //     // if reach this it is because the event type page has not been created, so it is not possible to revalidate it
+  //     console.debug((e as Error)?.message);
+  //   }
+  // }
+
+  revalidatePath('/services'); // revalidate the list page
+  revalidatePath(`/${user.username}/${updatedEventType.slug}`); // revalidate the service page
+  revalidatePath(`/${userProfile.username}/${updatedEventType.slug}`); // revalidate the service page
+  return {eventType};
+};
+
 
 // Delete
 
