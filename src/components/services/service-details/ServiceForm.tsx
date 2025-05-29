dab1c809d68d2f3dd3d097961d178d3aea3e3e9e
Benito Pedro <benitopedro21@outlook.com>
2025-05-28 23:33:21 -0300

    ✨ REMOVE: eliminate static form elements from ServiceForm component to integrate dynamic FormBuilder for booking questions #174

--- src/components/services/service-details/ServiceForm.tsx
+++ src/components/services/service-details/ServiceForm.tsx
@@ -33,34 +33,6 @@ export default function ServiceForm({slug}: Props) {
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="space-y-6 max-w-2xl">
-        <div className="space-y-4">
-          <div className="flex flex-col">
-            <div className="text-title-h6">Formulário da Reserva</div>
-            <div className="text-paragraph-sm text-text-sub-600">
-              Personalize as perguntas feitas na página de reservas
-            </div>
-          </div>
-          <div className="rounded-lg flex flex-col border border-stroke-soft-200">
-            <RequiredFormItem title="Nome" description="Digite o seu nome" />
-            <RequiredFormItem
-              title="E-mail"
-              description="Solicitar o e-mail do cliente"
-            />
-            <OptionalFormItem
-              title="Telefone"
-              description="Solicitar o telefone do cliente"
-              defaultChecked={true}
-            />
-            <OptionalFormItem
-              title="Participantes Adicionais"
-              description="Permitir adicionar e-mails de participantes adicionais"
-              defaultChecked={false}
-              showDivider={false}
-            />
-          </div>
-        </div>
-      </form>
       <FormBuilder
         title={t('booking_questions_title')}
         description={t('booking_questions_description')}
