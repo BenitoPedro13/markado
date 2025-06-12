b1b2787db90a52a402366d5541b609d217d5bbc9
Benito Pedro <benitopedro21@outlook.com>
2025-06-12 08:24:34 -0300

    REFINE: Standardize export formatting and ensure consistent module exports in index.ts #79

--- src/packages/lib/server/index.ts
+++ src/packages/lib/server/index.ts
@@ -1,1 +1,4 @@
-export { checkBookingLimits, checkBookingLimit } from "./checkBookingLimits";
+export {checkBookingLimits, checkBookingLimit} from './checkBookingLimits';
+
+export {defaultHandler} from './defaultHandler';
+export {defaultResponder} from './defaultResponder';
