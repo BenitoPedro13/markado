50447d4215fe632d76df24b415ac0957224d0a5c
Benito Pedro <benitopedro21@outlook.com>
2025-06-19 12:33:56 -0300

    ADD: Integrate next-i18next for internationalization support in useLocaleI18 hook #79

--- src/hooks/use-locale.ts
+++ src/hooks/use-locale.ts
@@ -14,3 +14,19 @@ export const useLocale = (
     isLocaleReady
   };
 };
+
+
+import {useTranslation} from 'next-i18next';
+
+export const useLocaleI18 = (
+  namespace: Parameters<typeof useTranslation>[0] = 'common'
+) => {
+  const {i18n, t} = useTranslation(namespace);
+  const isLocaleReady = Object.keys(i18n).length > 0;
+
+  return {
+    i18n,
+    t,
+    isLocaleReady
+  };
+};
