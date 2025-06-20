4562f03fe2afdfbd9153721e5a039cdfb8d27965
Benito Pedro <benitopedro21@outlook.com>
2025-06-20 09:39:45 -0300

    ADD: Export checkDurationLimit and checkDurationLimits functions from checkBookingLimits module #79

--- src/packages/lib/server/index.ts
+++ src/packages/lib/server/index.ts
@@ -1,4 +1,5 @@
 export {checkBookingLimits, checkBookingLimit} from './checkBookingLimits';
+export {checkDurationLimit, checkDurationLimits} from './checkDurationLimits'
 
 export {defaultHandler} from './defaultHandler';
 export {defaultResponder} from './defaultResponder';
