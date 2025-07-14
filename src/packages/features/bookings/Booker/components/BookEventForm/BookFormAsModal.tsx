dd4c923be11cac2534456a4c5f62f49b88752480
Benito Pedro <benitopedro21@outlook.com>
2025-07-14 13:17:40 -0300

    refactor: update BookEventFormAsModal component to improve structure and enhance UI elements

--- src/packages/features/bookings/Booker/components/BookEventForm/BookFormAsModal.tsx
+++ src/packages/features/bookings/Booker/components/BookEventForm/BookFormAsModal.tsx
@@ -1,57 +1,75 @@
-import type { ReactNode } from "react";
-import React from "react";
+import type {ReactNode} from 'react';
+import React from 'react';
 
-import { useEventTypeById, useIsPlatform } from "@/atoms/monorepo";
-import { useLocale } from "@/hooks/use-locale";
-import { Badge, Dialog, DialogContent } from "@/components/align-ui/ui/alert";
+// import { useEventTypeById, useIsPlatform } from "@/atoms/monorepo";
+import {useLocaleI18} from '@/hooks/use-locale';
+// import { Badge, Dialog, DialogContent } from "@/components/align-ui/ui/alert";
+import {RiCalendarLine, RiTimeLine} from '@remixicon/react';
+import {Root as Badge} from '@/components/align-ui/ui/badge';
+import * as Dialog from '@/components/align-ui/ui/modal';
+import * as Button from '@/components/align-ui/ui/button';
+import {getDurationFormatted} from '../../../components/event-meta/Duration';
+import {useTimePreferences} from '../../../lib';
+import {useBookerStore} from '../../store';
+import {FromTime} from '../../utils/dates';
+import {useEvent} from '../../utils/event';
 
-import { getDurationFormatted } from "../../../components/event-meta/Duration";
-import { useTimePreferences } from "../../../lib";
-import { useBookerStore } from "../../store";
-import { FromTime } from "../../utils/dates";
-import { useEvent } from "../../utils/event";
-
-const BookEventFormWrapper = ({ children, onCancel }: { onCancel: () => void; children: ReactNode }) => {
-  const { data } = useEvent();
-
-  return <BookEventFormWrapperComponent child={children} eventLength={data?.length} onCancel={onCancel} />;
-};
-
-const PlatformBookEventFormWrapper = ({
+const BookEventFormWrapper = ({
   children,
-  onCancel,
+  onCancel
 }: {
   onCancel: () => void;
   children: ReactNode;
 }) => {
-  const eventId = useBookerStore((state) => state.eventId);
-  const { data } = useEventTypeById(eventId);
+  const {data} = useEvent();
 
   return (
-    <BookEventFormWrapperComponent child={children} eventLength={data?.lengthInMinutes} onCancel={onCancel} />
+    <BookEventFormWrapperComponent
+      child={children}
+      eventLength={data?.length}
+      onCancel={onCancel}
+    />
   );
 };
 
+// const PlatformBookEventFormWrapper = ({
+//   children,
+//   onCancel,
+// }: {
+//   onCancel: () => void;
+//   children: ReactNode;
+// }) => {
+//   const eventId = useBookerStore((state) => state.eventId);
+//   const { data } = useEventTypeById(eventId);
+
+//   return (
+//     <BookEventFormWrapperComponent child={children} eventLength={data?.lengthInMinutes} onCancel={onCancel} />
+//   );
+// };
+
 export const BookEventFormWrapperComponent = ({
   child,
-  eventLength,
+  eventLength
 }: {
   onCancel: () => void;
   child: ReactNode;
   eventLength?: number;
 }) => {
-  const { i18n, t } = useLocale();
+  const {i18n, t} = useLocaleI18();
   const selectedTimeslot = useBookerStore((state) => state.selectedTimeslot);
   const selectedDuration = useBookerStore((state) => state.selectedDuration);
-  const { timeFormat, timezone } = useTimePreferences();
+  const {timeFormat, timezone} = useTimePreferences();
   if (!selectedTimeslot) {
     return null;
   }
   return (
     <>
-      <h1 className="font-cal text-emphasis text-xl leading-5">{t("confirm_your_details")} </h1>
+      <h1 className="font-cal text-emphasis text-xl leading-5">
+        {t('confirm_your_details')}{' '}
+      </h1>
       <div className="my-4 flex flex-wrap gap-2 rounded-md leading-none">
-        <Badge variant="grayWithoutHover" startIcon="calendar" size="lg">
+        <Badge variant="filled" color="gray" size="medium">
+          <RiCalendarLine className="h-4 w-4 mr-1" />
           <FromTime
             date={selectedTimeslot}
             timeFormat={timeFormat}
@@ -60,8 +78,16 @@ export const BookEventFormWrapperComponent = ({
           />
         </Badge>
         {(selectedDuration || eventLength) && (
-          <Badge variant="grayWithoutHover" startIcon="clock" size="lg">
-            <span>{getDurationFormatted(selectedDuration || eventLength, t)}</span>
+          <Badge
+            variant="filled"
+            color="gray"
+            // startIcon="clock"
+            size="medium"
+          >
+            <RiTimeLine className="h-4 w-4 mr-1" />
+            <span>
+              {getDurationFormatted(selectedDuration || eventLength, t)}
+            </span>
           </Badge>
         )}
       </div>
@@ -73,26 +99,27 @@ export const BookEventFormWrapperComponent = ({
 export const BookFormAsModal = ({
   visible,
   onCancel,
-  children,
+  children
 }: {
   visible: boolean;
   onCancel: () => void;
   children: ReactNode;
 }) => {
-  const isPlatform = useIsPlatform();
+  // const isPlatform = useIsPlatform();
+
+  // const isPlatform = false;
 
   return (
-    <Dialog open={visible} onOpenChange={onCancel}>
-      <DialogContent
-        type={undefined}
-        enableOverflow
-        className="[&_.modalsticky]:border-t-subtle [&_.modalsticky]:bg-default max-h-[80vh] pb-0 [&_.modalsticky]:sticky [&_.modalsticky]:bottom-0 [&_.modalsticky]:left-0 [&_.modalsticky]:right-0 [&_.modalsticky]:-mx-8 [&_.modalsticky]:border-t [&_.modalsticky]:px-8 [&_.modalsticky]:py-4">
-        {!isPlatform ? (
-          <BookEventFormWrapper onCancel={onCancel}>{children}</BookEventFormWrapper>
-        ) : (
-          <PlatformBookEventFormWrapper onCancel={onCancel}>{children}</PlatformBookEventFormWrapper>
-        )}
-      </DialogContent>
-    </Dialog>
+    <Dialog.Root open={visible} onOpenChange={onCancel}>
+      {' '}
+      {/* Modal de cancelamento */}
+      <Dialog.Content>
+        <Dialog.Body>
+          <BookEventFormWrapper onCancel={onCancel}>
+            {children}
+          </BookEventFormWrapper>
+        </Dialog.Body>
+      </Dialog.Content>
+    </Dialog.Root>
   );
 };
