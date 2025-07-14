9ac3e68ff3fe4dea06e01800207f8ac954349564
Benito Pedro <benitopedro21@outlook.com>
2025-07-14 12:46:56 -0300

    refactor: update InputField component to enhance localization support and improve code readability

--- src/components/align-ui/ui/text-field.tsx
+++ src/components/align-ui/ui/text-field.tsx
@@ -4,10 +4,11 @@ import React, { forwardRef, useId, useState } from "react";
 
 import {cn as classNames} from '@/utils/cn';
 // import { Icon } from "../../icon";
+import {RiCloseLine} from '@remixicon/react'
 // import { Skeleton } from "../../skeleton";
 // import { HintsOrErrors } from "./HintOrErrors";
-import {useLocale} from '@/hooks/use-locale';
-import * as Label from "@/components/align-ui/ui/label";
+import {useLocaleI18} from '@/hooks/use-locale';
+// import * as Label from "@/components/align-ui/ui/label";
 import type { InputFieldProps, InputProps } from "./form/types";
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
@@ -65,7 +66,7 @@ const Addon = ({ isFilled, children, className, error, onClickAddon }: AddonProp
 
 export const InputField = forwardRef<HTMLInputElement, InputFieldProps>(function InputField(props, ref) {
   const id = useId();
-  const { t: _t, isLocaleReady, i18n } = useLocale();
+  const {t: _t, isLocaleReady, i18n} = useLocaleI18();
   const t = props.t || _t;
   const name = props.name || "";
   const {
@@ -117,35 +118,40 @@ export const InputField = forwardRef<HTMLInputElement, InputFieldProps>(function
       {addOnLeading || addOnSuffix ? (
         <div
           dir="ltr"
-          className="focus-within:ring-brand-default group relative mb-1 flex items-center rounded-[10px] transition focus-within:outline-none focus-within:ring-2">
+          className="focus-within:ring-brand-default group relative mb-1 flex items-center rounded-[10px] transition focus-within:outline-none focus-within:ring-2"
+        >
           {addOnLeading && (
             <Addon
               isFilled={addOnFilled}
-              className={classNames("ltr:rounded-l-[10px] rtl:rounded-r-[10px]", addOnClassname)}>
+              className={classNames(
+                'ltr:rounded-l-[10px] rtl:rounded-r-[10px]',
+                addOnClassname
+              )}
+            >
               {addOnLeading}
             </Addon>
           )}
           <Input
-            data-testid={`${dataTestid}-input` ?? "input-field"}
+            // data-testid={`${dataTestid}-input` ?? "input-field"}
             id={id}
             type={type}
             placeholder={placeholder}
             isFullWidth={inputIsFullWidth}
             className={classNames(
               className,
-              "disabled:bg-subtle disabled:hover:border-subtle disabled:cursor-not-allowed",
-              addOnLeading && "rounded-l-none border-l-0",
-              addOnSuffix && "rounded-r-none border-r-0",
-              type === "search" && "pr-8",
-              "!my-0 !ring-0"
+              'disabled:bg-subtle disabled:hover:border-subtle disabled:cursor-not-allowed',
+              addOnLeading && 'rounded-l-none border-l-0',
+              addOnSuffix && 'rounded-r-none border-r-0',
+              type === 'search' && 'pr-8',
+              '!my-0 !ring-0'
             )}
             {...passThrough}
-            {...(type == "search" && {
-              onChange: (e) => {
+            {...(type == 'search' && {
+              onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
                 setInputValue(e.target.value);
                 props.onChange && props.onChange(e);
               },
-              value: inputValue,
+              value: inputValue
             })}
             disabled={readOnly || disabled}
             ref={ref}
@@ -154,17 +160,24 @@ export const InputField = forwardRef<HTMLInputElement, InputFieldProps>(function
             <Addon
               onClickAddon={onClickAddon}
               isFilled={addOnFilled}
-              className={classNames("ltr:rounded-r-[10px] rtl:rounded-l-[10px]", addOnClassname)}>
+              className={classNames(
+                'ltr:rounded-r-[10px] rtl:rounded-l-[10px]',
+                addOnClassname
+              )}
+            >
               {addOnSuffix}
             </Addon>
           )}
-          {type === "search" && inputValue?.toString().length > 0 && (
-            <Icon
-              name="x"
+          {type === 'search' && inputValue?.toString().length > 0 && (
+            <RiCloseLine
+              // name="x"
               className="text-subtle absolute top-2.5 h-4 w-4 cursor-pointer ltr:right-2 rtl:left-2"
               onClick={(e) => {
-                setInputValue("");
-                props.onChange && props.onChange(e as unknown as React.ChangeEvent<HTMLInputElement>);
+                setInputValue('');
+                props.onChange &&
+                  props.onChange(
+                    e as unknown as React.ChangeEvent<HTMLInputElement>
+                  );
               }}
             />
           )}
@@ -176,7 +189,7 @@ export const InputField = forwardRef<HTMLInputElement, InputFieldProps>(function
           placeholder={placeholder}
           className={classNames(
             className,
-            "disabled:bg-subtle disabled:hover:border-subtle disabled:cursor-not-allowed"
+            'disabled:bg-subtle disabled:hover:border-subtle disabled:cursor-not-allowed'
           )}
           {...passThrough}
           readOnly={readOnly}
@@ -185,8 +198,12 @@ export const InputField = forwardRef<HTMLInputElement, InputFieldProps>(function
           disabled={readOnly || disabled}
         />
       )}
-      <HintsOrErrors hintErrors={hintErrors} fieldName={name} t={t} />
-      {hint && <div className="text-default mt-2 flex items-center text-sm">{hint}</div>}
+      {/* <HintsOrErrors hintErrors={hintErrors} fieldName={name} t={t} /> */}
+      {hint && (
+        <div className="text-default mt-2 flex items-center text-sm">
+          {hint}
+        </div>
+      )}
     </div>
   );
 });
