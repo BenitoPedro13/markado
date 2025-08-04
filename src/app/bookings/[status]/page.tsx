5d3a0fbe4807fce1a0b8e53ee95d8c9480761de8
Benito Pedro <benitopedro21@outlook.com>
2025-08-04 06:39:50 -0300

    refactor: comment out unused imports and code in bookings page
    - Commented out various imports and code sections in the bookings page to clean up the file and improve readability.
    - This includes imports related to static props, metadata generation, and layout handling.

--- src/app/bookings/[status]/page.tsx
+++ src/app/bookings/[status]/page.tsx
@@ -1,25 +1,25 @@
-import { withAppDirSsg } from "@/app/WithAppDirSsg";
-import { generateMetadata as _generateMetadata } from "@/app/_utils";
-import { WithLayout } from "@/app/layoutHOC";
-import type { InferGetStaticPropsType } from "next";
+// import { withAppDirSsg } from "@/app/WithAppDirSsg";
+// import { generateMetadata as _generateMetadata } from "@/app/_utils";
+// import { WithLayout } from "@/app/layoutHOC";
+// import type { InferGetStaticPropsType } from "next";
 
-import { newValidStatuses } from "@/modules/bookings/lib/validStatuses";
-import ListPage from "@/modules/bookings/views/bookings-listing-view";
-import { getStaticProps } from "@/modules/bookings/views/bookings-listing-view.getStaticProps";
+// import { newValidStatuses } from "@/modules/bookings/lib/validStatuses";
+// import ListPage from "@/modules/bookings/views/bookings-listing-view";
+// import { getStaticProps } from "@/modules/bookings/views/bookings-listing-view.getStaticProps";
 
-type Y = InferGetStaticPropsType<typeof getStaticProps>;
-const getData = withAppDirSsg<Y>(getStaticProps);
+// type Y = InferGetStaticPropsType<typeof getStaticProps>;
+// const getData = withAppDirSsg<Y>(getStaticProps);
 
-export const generateMetadata = async () =>
-  await _generateMetadata(
-    (t) => t("bookings"),
-    (t) => t("bookings_description")
-  );
+// export const generateMetadata = async () =>
+//   await _generateMetadata(
+//     (t) => t("bookings"),
+//     (t) => t("bookings_description")
+//   );
 
-export const generateStaticParams = async () => {
-  return newValidStatuses.map((status) => ({ status }));
-};
+// export const generateStaticParams = async () => {
+//   return newValidStatuses.map((status) => ({ status }));
+// };
 
-export default WithLayout({ getLayout: null, getData, Page: ListPage })<"P">;
+// export default WithLayout({ getLayout: null, getData, Page: ListPage })<"P">;
 
-export const dynamic = "force-static";
+// export const dynamic = "force-static";
