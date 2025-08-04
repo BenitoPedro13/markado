52ebdf2df2e498e5f4be9c7e7bfb16f94af7e4e4
Amanda Jauregui <amandajauregui.dev@gmail.com>
2025-08-04 15:47:03 -0300

    fix: fix rescheduling email translations and added missing ones

--- src/packages/emails/templates/attendee-rescheduled-email.ts
+++ src/packages/emails/templates/attendee-rescheduled-email.ts
@@ -1,9 +1,17 @@
+import { getTranslation } from "@/packages/lib/server/i18n";
 import { renderEmail } from "../";
 import generateIcsFile, { GenerateIcsRole } from "../lib/generateIcsFile";
 import AttendeeScheduledEmail from "./attendee-scheduled-email";
 
 export default class AttendeeRescheduledEmail extends AttendeeScheduledEmail {
   protected async getNodeMailerPayload(): Promise<Record<string, unknown>> {
+    const locale = this.attendee.language.locale || 'pt';
+    const t = await getTranslation(locale, 'common');
+    
+    const subject = t("event_type_has_been_rescheduled_on_time_date", {
+      title: this.calEvent.title,
+    });
+
     return {
       icalEvent: generateIcsFile({
         calEvent: this.calEvent,
@@ -13,10 +21,7 @@ export default class AttendeeRescheduledEmail extends AttendeeScheduledEmail {
       to: `${this.attendee.name} <${this.attendee.email}>`,
       from: `${this.calEvent.organizer.name} <${this.getMailerOptions().from}>`,
       replyTo: [...this.calEvent.attendees.map(({ email }) => email), this.calEvent.organizer.email],
-      subject: `${this.attendee.language.translate("event_type_has_been_rescheduled_on_time_date", {
-        title: this.calEvent.title,
-        date: this.getFormattedDate(),
-      })}`,
+      subject,
       html: await renderEmail("AttendeeRescheduledEmail", {
         calEvent: this.calEvent,
         attendee: this.attendee,
